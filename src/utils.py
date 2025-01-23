c11775413407ca6e1c299bed657ac4833e9b530c
Taisha (artem) <sunlesslove128@gmail.com>
2025-01-23 22:55:38 +0500

    Прописал более совершенную логику движения, падения, рывка. Прописал класс анимации, прописал функцию получения списка фреймов, навесил анимации на прыжок, ходьбу, бездействие, и рывок, но рывок не работает, надо доделать, анимация почему-то прирывается Так же, возможно стоит сделать код чуть более чистым Но по сути, начальное управление готов

--- src/utils.py
+++ src/utils.py
@@ -23,3 +23,27 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+# Функция для получения всех кадров анмиации
+def load_animation(folder_path):
+    # Путь к папке, где лежат кадры
+    base_folder = os.path.join("../data/images", folder_path)
+
+    if not os.path.isdir(base_folder):
+        print(f"Папка {base_folder} не найдена")
+        sys.exit()
+
+    # Получаем все файлы в папке base_folder
+    files = sorted(os.listdir(base_folder))
+    frames = []
+
+    for file_name in files:
+        full_path = os.path.join(base_folder, file_name)
+        image = load_image(full_path, True)
+        image = pygame.transform.scale(image, (70, 80))
+        frames.append(image)
+
+    return frames
+
+
+
